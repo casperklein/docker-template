725dff523242acdfdaa6e9b73ab0b7e7228898c6
casperklein <casperklein@users.noreply.github.com>
2026-07-18 22:49:11 +0200

    build-go.sh

---
+++ build-go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ue
+
+#APP=""
+
+_cleanUp() {
+	docker rm  "$APP"
+	docker rmi "$APP"
+} &>/dev/null
+trap _cleanUp EXIT
+
+echo "Start building '$APP'"
+
+docker build --build-arg APP="$APP" --tag "$APP" --build-arg MULTIARCH="${1:-}" .
+docker create --name "$APP" "$APP"
+docker cp "$APP":/build/ .
+
+echo "Build was successful."
+echo
